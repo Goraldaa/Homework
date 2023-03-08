d3bd4aa15db47f6a66842e146d47f8566818eb67
Anhelina <goralda@yandex.ru>
2023-03-08 15:12:50 +0300

    fix spelling mistake in Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -2,5 +2,5 @@
 
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num % 2 == 0) Console.WriteLine($"Число {num} является чётное");
+if (num % 2 == 0) Console.WriteLine($"Число {num} является чётным");
 else Console.WriteLine($"Число {num} не является чётным");
