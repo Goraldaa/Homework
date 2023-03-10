0014a6a1de05309515a02cd8d91d1e4d01f9cd51
Anhelina <goralda@yandex.ru>
2023-03-10 15:34:34 +0300

    add task15

---
+++ Task15/Program.cs
@@ -0,0 +1,29 @@
+﻿// 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool DayOffCheck(int dayNumber)
+{
+    return dayNumber == 6 || dayNumber == 7;
+}
+
+bool DayCheck(int dayNumber)
+{
+    return dayNumber > 0 && dayNumber < 8;
+}
+
+Console.WriteLine("Введите номер дня недели");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+
+//такой вывод ещё премлем?
+
+Console.WriteLine(DayCheck(dayNum) ? (DayOffCheck(dayNum) ? "Это выходной": "Это не выходной") : "В нашем мире нет такого дня недели"); 
+
+// или лучше так?
+
+// if(DayCheck(dayNum))                                                                       
+//     Console.WriteLine(DayOffCheck(dayNum) ? "Это выходной": "Это не выходной");
+// else
+//     Console.WriteLine("В нашем мире нет такого дня недели");
