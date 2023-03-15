bcd9d82247941e946459ef6e67d8c199dc9ad9c5
Anhelina <goralda@yandex.ru>
2023-03-15 22:27:48 +0300

    add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+bool Poli(int number, int size)
+{
+    int i = 1;
+    while (i < size)
+    {
+        int pow = Convert.ToInt32(Math.Pow(10, size - i));
+        int firstDigit = number /  pow ;
+        int lastDigit = number % 10;
+        if (firstDigit != lastDigit) return false;
+        else number = (number - firstDigit * pow) / 10;
+        i += 2;
+    }
+    return true;
+}
+
+
+Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+int size = num[0] == '-'? num.Length - 1 : num.Length;
+int number = Convert.ToInt32(num);
+if(number < 0) number *= -1;
+
+
+Console.WriteLine(Poli(number, size) ? "да" : "нет");
+
