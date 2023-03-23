421e79b34844401dd0be58d15d573f20c59e7167
Anhelina <goralda@yandex.ru>
2023-03-23 10:03:06 +0300

    add task 32

---
+++ Task32/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArray(int arrayLength, int min, int max)
+{
+    int[] newArray = new int[arrayLength];
+
+    Random random = new Random();
+
+    for (int i = 0; i < arrayLength; i++)
+    {
+        newArray[i] = random.Next(min, max + 1);
+    }
+
+    return newArray;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+void Multiplication(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимально возможное число в массиве:");
+int minNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимально возможное число в массиве:");
+int maxNum = Convert.ToInt32(Console.ReadLine());
+
+// if (arraySize <= 0)
+// {
+//     Console.WriteLine($"Ошибка: введено неверное значение {arraySize}");
+//     return;
+// }
+
+int[] arr = CreateArray(arraySize, minNum, maxNum);
+
+PrintArray(arr);
+Multiplication(arr);
+Console.Write(" -> ");
+PrintArray(arr);
+
