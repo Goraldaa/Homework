b781d2390efccee817c92c083ba25ac8361b3756
Anhelina <goralda@yandex.ru>
2023-03-31 09:24:45 +0300

    add task46

---
+++ Task46/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] CreateMatrixmatrix(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+
+    Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4} ");
+        }
+
+        Console.Write("|");
+        Console.WriteLine();
+
+    }
+}
+int[,] array2d = CreateMatrixmatrix(3, 4, 1, 10);
+PrintMatrix(array2d);
