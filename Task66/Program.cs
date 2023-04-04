38679e7f5e262d4b0195c0ac31aeb9ae556e41a6
Anhelina <goralda@yandex.ru>
2023-04-04 18:49:36 +0300

    add task66

---
+++ Task66/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int SumNumberBetweenAB(int numberA, int numberB)
+{
+    if (numberA == numberB) return numberB;
+    else return numberA + SumNumberBetweenAB(numberA + 1, numberB);
+}
+bool Verification(int numberA, int numberB)
+{
+    return (numberA < 1 || numberB < 0);
+}
+
+
+Console.WriteLine("Введите целое положительное число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int sumNumber = 0;
+if(Verification(numA, numB)) 
+{
+    Console.WriteLine("Введены не натуральные числа");
+    return;
+}
+if (numA == numB)
+{
+    Console.WriteLine("Числа равны");
+    return;
+}
+if (numA < numB)
+    sumNumber = SumNumberBetweenAB(numA, numB);
+
+if (numA > numB)
+    sumNumber = SumNumberBetweenAB(numB,numA);
+
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {numA} до {numB} равна {sumNumber}");
